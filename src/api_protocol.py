1efaf82aa32a4705667fb01236d1dc53496d8e75
Dogan Hasanoglu <dcan.hasanoglu@tum.de>
2023-06-24 15:58:58 +0200

    api_protocol update for testing incoming connections

--- src/api_protocol.py
+++ src/api_protocol.py
@@ -1,14 +1,32 @@
 from dht_service import Service
 import asyncio
+import struct
 
 
 class ApiProtocol(asyncio.Protocol):
-    def __init__(self):
+    def init(self):
         """
         Initialize API protocol with a Service instance and a placeholder for transport.
         """
         self.service_handler = Service()
         self.data_transport = None
+
+    def data_received(self, data):
+        header = data[:4]
+        body = data[4:]
+
+        msize = struct.unpack(">HH", header)[0]
+
+        ttl, rep, res = struct.unpack(">HBB", data[4:8])
+
+        print(msize, ttl, rep, res)
+
+        key = data[8: int((256 / 8) + 8)]
+        print("key length", len(key))
+        val = data[int(8 + (256 / 8)):]
+
+        print(f"DHT_PUT: ({key}:{val})")
+        return True
 
     def establish_connection(self, transport):
         """
