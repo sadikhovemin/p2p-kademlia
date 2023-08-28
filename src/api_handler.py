d63c093f031314c627b900701378ea6b389d4670
Dogan Hasanoglu <dcan.hasanoglu@tum.de>
2023-08-28 21:57:28 +0200

    Update api_handler.py of seamless k-bucket operations.

--- src/api_handler.py
+++ src/api_handler.py
@@ -2,6 +2,8 @@ import asyncio
 
 from dht_service import Service
 from loguru import logger
+import struct
+
 
 class Handler(asyncio.Protocol):
     def __init__(self, ip, port, node, initiator=False, data=None, put=False, get=False):
@@ -14,6 +16,7 @@ class Handler(asyncio.Protocol):
         self.data = data
         self.put = put
         self.get = get
+        self.buffer = None
 
     def connection_made(self, transport):
         # print("connection made executed")
@@ -28,42 +31,28 @@ class Handler(asyncio.Protocol):
             self.transport.write(message)
 
         if self.put:
-            # print("connection_made put'a geldim")
             logger.info("connection_made put'a geldim")
             self.transport.write(self.data)
             self.close_connection()
 
         if self.get:
-            # print("connection_made get'e geldim")
-            # self.transport.write(self.data)
-
             if self.data is not None:
                 self.transport.write(self.data)
             else:
                 logger.error("Error: Result is None")
-                # print("Error: Result is None")
-        # if self.get:
-        #     print("connection_made get'e geldim")
-        #     self.transport.write(self.data)
-        #     self.close_connection()
 
     def data_received(self, data):
-        if len(data) == 0:
-            # print("No response")
-            logger.warning("No response")
+        self.buffer = b''
+        self.buffer += data  # Buffer the incoming data
 
+        size = struct.unpack(">H", self.buffer[:2])[0]
+        if len(data) == 0 or len(self.buffer) < size:
             return
 
-        # print("data received called")
-        logger.info("data received called")
+        complete_message = self.buffer[:size]
+        self.buffer = self.buffer[size:]
 
-        # if data == "ok".encode():
-        #     print("so far works")
-        #     return
-        #
-        # result = self.service.process_message(data)
-        # print("result", result)
-        # self.transport.write(result)
+        logger.info("data received called")
 
         if data == "ok".encode():
             # print("so far works")
@@ -72,31 +61,26 @@ class Handler(asyncio.Protocol):
             return
 
         if data == "Data stored successfully".encode():
-            # print("successfully stored the put request")
             logger.info("successfully stored the put request")
             return
 
         if data == "put calisti".encode():
-            # print("put works. no problems")
             logger.info("put works. no problems")
             return
 
         if data == "get calisti".encode():
-            # print("get works.")
             logger.info("get works.")
             return
 
         if data == "Key not found".encode():
-            # print("Key not found")
             logger.info("Key not found")
             return
 
         if data == "find_value_service calisti".encode():
-            # print("find_value_service calisti")
             logger.info("find_value_service calisti")
             return
 
-        asyncio.create_task(self.handle_response(data))
+        asyncio.create_task(self.handle_response(complete_message))
 
     async def handle_response(self, data):
         result = await self.service.process_message(data)
@@ -126,8 +110,10 @@ class Handler(asyncio.Protocol):
 
     async def put_connection(self, host, port, msg):
         loop = asyncio.get_event_loop()
-        await loop.create_connection(lambda: Handler(self.ip, self.port, self.node, initiator=False, data=msg, put=True), host, port)
+        await loop.create_connection(
+            lambda: Handler(self.ip, self.port, self.node, initiator=False, data=msg, put=True), host, port)
 
     async def get_connection(self, host, port, msg):
         loop = asyncio.get_event_loop()
-        await loop.create_connection(lambda: Handler(self.ip, self.port, self.node, initiator=False, data=msg, get=True), host, port)
+        await loop.create_connection(
+            lambda: Handler(self.ip, self.port, self.node, initiator=False, data=msg, get=True), host, port)
